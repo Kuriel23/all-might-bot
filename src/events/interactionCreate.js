107a4877f54db8906a2aeb00b2b253eba5d73d72
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-05-24 00:12:40 +0100

    feat: remove highest role system

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -6,26 +6,15 @@ module.exports = (client, interaction) => {
 		require("../button/" + interaction.customId)(client, interaction);
 	}
 	if (interaction.isChatInputCommand()) {
-		if (
-			interaction.member.roles.highest.comparePositionTo(
-				interaction.guild.members.fetch(client.user.id).roles.highest
-			) > 0
-		) {
-			const command = client.commands.get(interaction.commandName);
-			if (!command) return;
-			try {
-				command.execute(interaction, client);
-			} catch (err) {
-				if (err) console.error(err);
-				interaction.reply({
-					content: "Um erro foi executado no meu grande algoritmo.",
-					ephemeral: true,
-				});
-			}
-		} else {
+		const command = client.commands.get(interaction.commandName);
+		if (!command) return;
+		try {
+			command.execute(interaction, client);
+		} catch (err) {
+			if (err) console.error(err);
 			interaction.reply({
-				content:
-					"Não se preocupe, jovem. Você não precisa de comandos para ser um verdadeiro herói.",
+				content: "Um erro foi executado no meu grande algoritmo.",
+				ephemeral: true,
 			});
 		}
 	}
